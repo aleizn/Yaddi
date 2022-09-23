bde8ef398942c119136483b8f96ba88f0c15e928
VIRUZOFC <113963745+VIRUZOFC@users.noreply.github.com>
2022-09-23 02:02:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DORRAT - Bot\nWhatsApp Bot MD', {
+say('DORRAT - Bot\nWhatsApp Bot MD modificafo por VIRUZZ-OFC', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
